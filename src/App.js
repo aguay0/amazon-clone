d391adf03f0631f28e348757c87442f041f56511
alfredo aguayo <121626040+aguay0@users.noreply.github.com>
2025-08-12 06:13:55 -0500

    Added front end part of login page

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import Header from './Header';
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Checkout from "./Checkout";
+import Login from "./Login"
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
         <Routes>
+          <Route path="/login" element={<Login/>} />
 
-          <Route path="/checkout" element={<Checkout/>} />
+          <Route path="/checkout" element={[<Header />,<Checkout/>]} />
 
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={[<Header />,<Home />]} />
 
         </Routes>
       </div>
