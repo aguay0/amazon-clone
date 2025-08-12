027ad7748c06294c79fd7b1de7908b9a375098c3
alfredo aguayo <121626040+aguay0@users.noreply.github.com>
2025-08-11 21:06:03 -0500

    Worked on checkout page. Added state provider and began basket implementation

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from "react-router-dom";
+import { useStateValue } from './StateProvider';
 
 function Header() {
+    const [ {basket}, dispatch] = useStateValue();
   return (
     <div className='header'>
-        <img className="header_logo" src="https://static.vecteezy.com/system/resources/thumbnails/019/766/240/small/amazon-logo-amazon-icon-transparent-free-png.png"/>
-
+        <Link to="/">
+            <img className="header_logo" src="https://static.vecteezy.com/system/resources/thumbnails/019/766/240/small/amazon-logo-amazon-icon-transparent-free-png.png"/>
+        </Link>
+        
         <div className="header_search">
             <input className="header_searchInput" type="text" />
             <SearchIcon className="header_searchIcon"/>            
@@ -29,10 +34,12 @@ function Header() {
                 <span className="header_optionLineOne">Your</span>
                 <span className="header_optionLineTwo">Prime</span>
             </div>
-            <div className="header_optionBasket">
-                <ShoppingBasketIcon />
-                <span className="headeroptionLineTwo header_basketCount">0</span>
-            </div>
+            <Link to="/checkout">
+                <div className="header_optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="headeroptionLineTwo header_basketCount">{basket?.length}</span>
+                </div>
+            </Link>
         </div>
     </div>
   )
--- src/Home.js
+++ src/Home.js
@@ -9,22 +9,53 @@ function Home() {
             <img className="home_image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="" />
             <div className="home_row">
                 <Product 
-                title="The Lean startup"
+                id="12321341"
+                title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
                 price={29.99}
                 image="https://m.media-amazon.com/images/I/71sxTeZIi6L._UF1000,1000_QL80_.jpg"
                 rating={5}
                 />
-                <Product />
+                <Product 
+                id="49538094"
+                title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+                price={239.00}
+                rating={4}
+                image="https://www.remodelista.com/wp-content/uploads/2015/03/fields/kenwood-kmx54-k-mix-kitchen-machine-electric-stand-mixer-remodelista.jpg"
+                />
             </div>
 
             <div className="home_row">
-                <Product />
-                <Product />
-                <Product />
+                <Product 
+                id="4903850"
+                title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+                price={199.99}
+                rating={3}
+                image="https://image-us.samsung.com/SamsungUS/home/computing/monitors/gaming/12282023/gal/G55C_003_R-Perspective_Black_PC_1600x1200.jpg?$product-details-jpg$"
+                />
+                <Product 
+                id="23445930"
+                title="Amazon Echo (3rd Generation) | Smart speaker with Alexa, Charcoal Fabric"
+                price={98.99}
+                rating={5}
+                image="https://www.bhphotovideo.com/images/fb/amazon_b07nftvp7p_echo_3rd_generation_charcoal_1512895.jpg"
+                />
+                <Product 
+                id="3254354345"
+                title="New Apple iPad pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+                price={598.99}
+                rating={4}
+                image="https://m.media-amazon.com/images/I/81SGb5l+lZL._UF1000,1000_QL80_.jpg"
+                />
             </div>
 
             <div className="home_row">
-                <Product />
+                <Product 
+                id="90829332"
+                title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+                price={1094.98}
+                rating={4}
+                image="https://m.media-amazon.com/images/I/81gf+wgrcfS.jpg"
+                />
             </div>
         </div>
     </div>
